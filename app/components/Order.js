165fdf889013a75f4f73e29e10656e8852330749
Douglas Maringa <douglasmaringa4@gmail.com>
2023-11-21 10:12:14 +0200

    first commit

--- app/components/Order.js
+++ app/components/Order.js
@@ -12,7 +12,7 @@ function Order() {
   
   return (
     <div className='max-w-3xl mx-auto mt-20'>
-      <h1 className="text-3xl text-center font-semibold text-[#5B20B6] mb-6">Your Orders</h1>
+      <h1 className="text-3xl text-center font-semibold text-[#5B20B6] mb-6">Your Orders Page</h1>
 
       <table className="w-full border-collapse">
         <thead>
